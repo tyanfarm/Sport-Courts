761bbf9c7f9214c53f23bbbaa6dd2c01e5843b93
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-06-06 17:26:24 +0700

    add Login page

--- FE/customer/src/App.js
+++ FE/customer/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
       <Header />
       <Home/>
+      <Login/>
       <Footer/>
     </div>
   );
