9c785035acc4eb4476da814bb5759cb8f003b72f
NTTrinh2003 <trtrinh212003@gmail.com>
2024-06-11 19:53:22 +0700

    Try fetch Category data from api, but failed

--- BE/obj/Debug/net7.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net7.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604a04c13305a42a6d9da5fe3720f5ca0b6dc5ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
