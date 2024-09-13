98ba51f43b3bad21d372b8593f6aed948c3184a7
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-09-13 15:24:56 +0700

    add scrollTop in MessageContainer FE

--- FE/customer/src/components/Chat/MessageContainer.js
+++ FE/customer/src/components/Chat/MessageContainer.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+// useRef là 1 hook để lưu trữ và truy cập trực tiếp các phần tử DOM mà không cần re-render cả page
 
 const MessageContainer = ({ messages }) => {
-  return (
-    <div className='message-container'>
-        {messages.map((m, index) => 
-            <div key={index} className={`user-message ${m.type}`}>
-                {m.message ? (
-                    <div className={`message ${m.type === 'sender' ? 'bg-primary' : (m.type === 'others' ? 'bg-secondary' : '')}`}>
-                        {m.message}
-                    </div>
-                ) : (
-                    <div className={`message-file ${m.type}`}>
-                        <img src={m.image} alt="Sent Image" className="message-image" />
-                    </div>
-                )}
+    const chatContainerRef = useRef(null);
 
-                <div className='from-user'>{m.type === 'join' ? '' : m.user}</div>
-            </div>
-        )}
-    </div>
-  )
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+            // đặt `scrolllTop = scrollHeight` để cuộn phần tử xuống cuối
+        }
+    }, [messages]);
+
+
+    return (
+        <div
+            ref={chatContainerRef}
+            className='message-container'
+        >
+            {messages.map((m, index) =>
+                <div key={index} className={`user-message ${m.type}`}>
+                    {m.message ? (
+                        <div className={`message ${m.type === 'sender' ? 'bg-primary' : (m.type === 'others' ? 'bg-secondary' : '')}`}>
+                            {m.message}
+                        </div>
+                    ) : (
+                        <div className={`message-file ${m.type}`}>
+                            <img src={m.image} alt="Sent Image" className="message-image" />
+                        </div>
+                    )}
+
+                    <div className='from-user'>{m.type === 'join' ? '' : m.user}</div>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default MessageContainer;
